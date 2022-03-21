e39f310e1a5a267a2b88ed3aa6073f481cf997ce
Rasujon3 <rasujon3@gmail.com>
2022-03-22 04:12:52 +0600

    createing navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
