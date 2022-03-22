cb02387756d6117420240c846a9b9386d2b3f2e7
Rasujon3 <rasujon3@gmail.com>
2022-03-23 04:05:32 +0600

    185. Fetching Data From Try And Catch

--- src/context.js
+++ src/context.js
@@ -1,5 +1,41 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, useCallback } from "react";
+
+const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
 
 const AppProvider = () => {
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("s");
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      console.log(data);
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlchoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchTerm]);
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
 };
