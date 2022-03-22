e9d2abbccfa33735228497a144ffed1da9bd2ab8
Rasujon3 <rasujon3@gmail.com>
2022-03-23 03:39:04 +0600

    creating the context

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        {/* <Route exact path='/' >  </Route> */}
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route path="/about">
           <About />
         </Route>
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,22 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About Me</h1>
+      <p>
+        I am Ruhul Amin Sujon . My home district is Gaibandha. I graduated with
+        B.Sc in CSE from Sonargaon University, Dhaka. My CGPA is 3.6 out of 4. I
+        achieved some professional courses from bohubrihi & programming hero.
+        Coming to my family we are now 4 members: Including me. My father was a
+        police officer. my mother is a housewife. I have an elder brother, he
+        recently join NRBC bank as an accounts officer. and I have also a
+        younger sister & she studying at degree final year. My hobby is
+        traveling. My strength is a positive attitude, quick learning,
+        confidence. My weakness is my sweetness family. I have done some
+        projects like portfolio and e-commerce projects and I still learning.
+        Thank you.
+      </p>
+    </section>
   );
 };
 
